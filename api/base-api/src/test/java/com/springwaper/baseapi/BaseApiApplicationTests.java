a58f48b63d18963681f2104228895152ccf3c656
waper97 <563667426@qq.com>
2021-06-22 17:31:09 +0800

    创建项目

---
+++ api/base-api/src/test/java/com/springwaper/baseapi/BaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwaper.baseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
